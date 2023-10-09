8251fe7704095e93322fd836d127556912814c20
Mohamed Kamal <mohamedkamal.ka@gmail.com>
2023-10-09 20:41:34 +0300

    check

--- lib/views/Desktop/screens/Edit Screen/toshiba/freezer_edit.dart
+++ lib/views/Desktop/screens/Edit Screen/toshiba/freezer_edit.dart
@@ -4,16 +4,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:warehouse/Routes/rutes_name.dart';
-<<<<<<< HEAD
-import 'package:warehouse/buttomsheet/deep_18.dart';
-import 'package:warehouse/buttomsheet/deep_22.dart';
-=======
 import 'package:warehouse/buttomsheet/add_deep18.dart';
 import 'package:warehouse/buttomsheet/add_deep22.dart';
 import 'package:warehouse/buttomsheet/sell_deep18.dart';
 import 'package:warehouse/buttomsheet/sell_deep22.dart';
 import 'package:warehouse/custom/custom_add_sell_row.dart';
->>>>>>> 7caeef1c850326ab280d9ad491894c0fec9b0fb8
 
 class FreezerToshibaEdit extends StatefulWidget {
   const FreezerToshibaEdit({super.key});
